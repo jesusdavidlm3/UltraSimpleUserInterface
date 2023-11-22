7e219b0a68dc6937283ca5c64154471405256580
jesusdavidlm3 <jesusdavidlozanomarin@gmail.com>
2023-11-22 16:30:44 -0400

    Cambios
    agregado el boton toggle button y empezados a aplicar sus estilos

--- src/components/CheckBox/CheckBox.jsx
+++ src/components/CheckBox/CheckBox.jsx
@@ -2,9 +2,9 @@ import './CheckBoxStyle.css'
 
 export const CheckBox = ({ label }) => {
     return(
-        <>
+        <div className='chckbxctn'>
             <input className='checkBox' type="checkbox"></input>
             <p>{ label }</p>
-        </>
+        </div>
     )
 }
